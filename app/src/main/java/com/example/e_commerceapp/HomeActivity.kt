901918d36d09a3e72fb9f33ae8f9ff73e431cac5
MbesarMbaya <roymbaya08@gmil.com>
2022-08-31 13:03:38 +0300

    commit

--- app/src/main/java/com/example/e_commerceapp/HomeActivity.kt
+++ app/src/main/java/com/example/e_commerceapp/HomeActivity.kt
@@ -1,11 +1,23 @@
 package com.example.e_commerceapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+//        button intent
+        val text = findViewById<TextView>(R.id.back_intent)
+
+        text.setOnClickListener {
+            val try_intent = Intent(this, SplashActivity::class.java)
+            startActivity(try_intent)
+        }
+
     }
 }
